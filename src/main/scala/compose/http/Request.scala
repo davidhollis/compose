c5118326679f2ce54ad5614b9681fc2635e586be
David Hollis <david@davidhollis.net>
2020-06-13 14:50:04 -0700

    Move status and method constants

--- src/main/scala/compose/http/Request.scala
+++ src/main/scala/compose/http/Request.scala
@@ -10,13 +10,15 @@ case class Request(
 
 object Request {
   sealed abstract class Method(override val toString: String)
-  case object Get extends Method("GET")
-  case object Head extends Method("HEAD")
-  case object Post extends Method("POST")
-  case object Put extends Method("PUT")
-  case object Delete extends Method("DELETE")
-  case object Connect extends Method("CONNECT")
-  case object Options extends Method("OPTIONS")
-  case object Trace extends Method("TRACE")
-  case object Patch extends Method("PATCH")
+  object Method {
+    case object Get extends Method("GET")
+    case object Head extends Method("HEAD")
+    case object Post extends Method("POST")
+    case object Put extends Method("PUT")
+    case object Delete extends Method("DELETE")
+    case object Connect extends Method("CONNECT")
+    case object Options extends Method("OPTIONS")
+    case object Trace extends Method("TRACE")
+    case object Patch extends Method("PATCH")
+  }
 }
