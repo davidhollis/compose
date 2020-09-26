59892147bb618737c6e71284728bbd07021ee24f
David Hollis <david@davidhollis.net>
2020-09-26 15:50:11 -0700

    Enable updating attribute lists

--- core/src/main/scala/compose/http/attributes/HasAttr.scala
+++ core/src/main/scala/compose/http/attributes/HasAttr.scala
@@ -1,6 +1,6 @@
 package compose.http.attributes
 
-import scala.annotation.{ showAsInfix, unused }
+import scala.annotation.{ implicitNotFound, showAsInfix }
 
 /** Evidence that a given attribute list has an element of a specific type.
   *
@@ -10,11 +10,12 @@ import scala.annotation.{ showAsInfix, unused }
   *   the element type
   */
 @showAsInfix
-trait HasAttr[Attrs <: AttrList, A]
+@implicitNotFound("Could not prove that ${Attrs} contains a value of type ${A}")
+trait HasAttr[Attrs <: AttrList, A] {
+  def update(list: Attrs, tag: AttrTag[A], op: A => A): Attrs
+}
 
-object HasAttr {
-
-  private def apply[Attrs <: AttrList, A](): HasAttr[Attrs, A] = new HasAttr[Attrs, A] {}
+trait HasAttrLowPriorityImplicits {
 
   /** Evidence that an attribute list contains an element of a specific type if the head of the list
     * is an element of that type.
@@ -24,7 +25,16 @@ object HasAttr {
     * @tparam Rest
     *   the type of the attribute list's tail
     */
-  implicit def headHasAttr[T, Rest <: AttrList]: HasAttr[Attr[T, Rest], T] = HasAttr()
+  implicit def headHasAttr[T, Rest <: AttrList]: HasAttr[Attr[T, Rest], T] =
+    new HasAttr[Attr[T, Rest], T] {
+
+      def update(list: Attr[T, Rest], tag: AttrTag[T], op: T => T): Attr[T, Rest] =
+        if (list.tag == tag)
+          list.copy(value = op(list.value))
+        else
+          list
+
+    }
 
   /** Evidence that an attribute list contains an element of a specific type if we have evidence
     * that the tail contains an attribute of that type.
@@ -40,7 +50,33 @@ object HasAttr {
     */
   implicit def tailHasAttr[T, Rest <: AttrList, A](
     implicit
-    @unused ev: HasAttr[Rest, A]
-  ): HasAttr[Attr[T, Rest], A] = HasAttr()
+    ev: Rest HasAttr A
+  ): HasAttr[Attr[T, Rest], A] =
+    new HasAttr[Attr[T, Rest], A] {
+
+      def update(list: Attr[T, Rest], tag: AttrTag[A], op: A => A): Attr[T, Rest] =
+        list.copy(rest = ev.update(list.rest, tag, op))
+
+    }
 
 }
+
+trait HasAttrHighPriorityImplicits extends HasAttrLowPriorityImplicits {
+
+  implicit def headAndTailBothHaveAttr[T, Rest <: AttrList](
+    implicit
+    ev: Rest HasAttr T
+  ): HasAttr[Attr[T, Rest], T] =
+    new HasAttr[Attr[T, Rest], T] {
+
+      def update(list: Attr[T, Rest], tag: AttrTag[T], op: T => T): Attr[T, Rest] =
+        if (list.tag == tag)
+          list.copy(value = op(list.value))
+        else
+          list.copy(rest = ev.update(list.rest, tag, op))
+
+    }
+
+}
+
+object HasAttr extends HasAttrHighPriorityImplicits
--- core/src/test/scala/compose/http/attributes/AttrListSpec.scala
+++ core/src/test/scala/compose/http/attributes/AttrListSpec.scala
@@ -71,4 +71,83 @@ class AttrListSpec extends compose.Spec {
       }
     }
   }
+
+  "Updates on attribute lists" should {
+    "modify the correct attribute" when {
+      "the type is unique and the selected tag is the head" in {
+        val attrs = Attr(IntTag1, 19, Attr(StringTag, "nineteen", NoAttrs))
+        val updated =
+          implicitly[Attr[Int, Attr[String, NoAttrs]] HasAttr Int]
+            .update(attrs, IntTag1, (i => (i * 100) + 99))
+
+        updated(IntTag1) should equal(Some(1999))
+      }
+
+      "the type is unique and the selected tag is in the tail" in {
+        val attrs = Attr(IntTag1, 19, Attr(StringTag, "nineteen", NoAttrs))
+        val updated =
+          implicitly[Attr[Int, Attr[String, NoAttrs]] HasAttr String]
+            .update(attrs, StringTag, century => century + " ninety-nine")
+
+        updated(StringTag) should equal(Some("nineteen ninety-nine"))
+      }
+
+      "the type is not unique and the selected tag is first" in {
+        val attrs = Attr(IntTag1, 39, Attr(IntTag2, 43, NoAttrs))
+        val updated =
+          implicitly[Attr[Int, Attr[Int, NoAttrs]] HasAttr Int].update(attrs, IntTag1, i => i - 12)
+
+        updated(IntTag1) should equal(Some(27))
+        updated(IntTag2) should equal(Some(43))
+      }
+
+      "the type is not unique and the selected tag is not first" in {
+        val attrs = Attr(IntTag1, 39, Attr(IntTag2, 43, NoAttrs))
+        val updated =
+          implicitly[Attr[Int, Attr[Int, NoAttrs]] HasAttr Int].update(attrs, IntTag2, i => i - 12)
+
+        updated(IntTag1) should equal(Some(39))
+        updated(IntTag2) should equal(Some(31))
+      }
+
+      "the tag is not unique" in {
+        // Specifically, if the tag is not unique, `update` should modfiy only the leftmost (most
+        // recently added) value with the selected tag. This is because only that one is selectable
+        // using AttrTag[T].unapply
+        val attrs = Attr(IntTag1, 39, Attr(IntTag1, 43, NoAttrs))
+        val updated =
+          implicitly[Attr[Int, Attr[Int, NoAttrs]] HasAttr Int].update(attrs, IntTag1, i => i * 5)
+
+        updated should equal(Attr(IntTag1, 195, Attr(IntTag1, 43, NoAttrs)))
+      }
+    }
+
+    "fail to modify anything" when {
+      "the type is not present" in {
+        // It shouldn't even compile if a value of the selected tag's type isn't there
+        """
+          |import compose.http.attributes._
+          |object FloatTag extends AttrTag[Float]("test float tag")
+          |object BooleanTag extends AttrTag[Boolean]("test boolean tag")
+          |object CharTag extends AttrTag[Char]("test char tag")
+          |val attrs = Attr(FloatTag, 1.0f, Attr(BooleanTag, true, NoAttrs))
+          |val updated =
+          |  implicitly[Attr[Float, Attr[Boolean, NoAttrs]] HasAttr Char].update(attrs, CharTag, identity[Char])
+        """.stripMargin shouldNot typeCheck
+      }
+
+      "the tag is not present" in {
+        object FloatTag1 extends AttrTag[Float]("test float tag 1")
+        object FloatTag2 extends AttrTag[Float]("test float tag 2")
+        object FloatTag3 extends AttrTag[Float]("test float tag 3")
+        object FloatTag4 extends AttrTag[Float]("test float tag 4")
+
+        val attrs = Attr(FloatTag1, 1.0f, Attr(FloatTag2, 2.0f, Attr(FloatTag3, 3.0f, NoAttrs)))
+        val updated = implicitly[Attr[Float, Attr[Float, Attr[Float, NoAttrs]]] HasAttr Float]
+          .update(attrs, FloatTag4, _ => 0.0f)
+
+        updated should equal(attrs)
+      }
+    }
+  }
 }
