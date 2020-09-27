bcd4bce433a49577eede5cdd81d98f223bc9fe2a
David Hollis <david@davidhollis.net>
2020-09-26 22:40:29 -0700

    Fix linter complaints about asInstanceOf

--- core/src/main/scala/compose/http/attributes/AttrMap.scala
+++ core/src/main/scala/compose/http/attributes/AttrMap.scala
@@ -2,8 +2,13 @@ package compose.http.attributes
 
 class AttrMap(underlying: Map[Any, Any] = Map.empty) {
   def contains[T](key: AttrKey[T]): Boolean = underlying.contains(key)
+
+  @SuppressWarnings(Array("scalafix:DisableSyntax.asInstanceOf"))
   def apply[T](key: AttrKey[T]): T = underlying(key).asInstanceOf[T]
+
+  @SuppressWarnings(Array("scalafix:DisableSyntax.asInstanceOf"))
   def get[T](key: AttrKey[T]): Option[T] = underlying.get(key).asInstanceOf[Option[T]]
+
   def getOrElse[T, T1 >: T](key: AttrKey[T], default: => T1): T1 = get(key).getOrElse[T1](default)
   def +[T](kv: (AttrKey[T], T)): AttrMap = new AttrMap(underlying + kv)
   def -[T](key: AttrKey[T]): AttrMap = new AttrMap(underlying - key)
